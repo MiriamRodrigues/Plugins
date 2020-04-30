c625eab5bb05a24dd0be90f96f623b3333daa7c0
Miriam Rodrigues <miriambrodrigues2@gmail.com>
2020-04-30 17:55:48 -0300

    Add files via upload

---
+++ bmi_calc.php
@@ -0,0 +1,48 @@
+<!-- hidden plugin folder path -->
+<span id="hiddenppath" data-Path="<?php echo plugins_url(); ?>">Kg/Cm</span>
+
+<div class="bootstrap-scope">
+    <form id="bmibmrCalc" method="post" action="<?php echo plugins_url('bmibmr_calculations.php', __FILE__); ?>" >
+        <div class="row">
+            <div id="data-panel" class="col-md-8 well">
+                <div class="col-md-12">
+                    <input class="hidden" type="radio" name="uVal" value="kg" checked required>
+
+                    <div class="form-group">
+                        <label class="control-label" for="gender">Gênero</label>
+                        <div class="input-group" style="width:100%">
+                            <div class="btn-group radio-group" style="width: 100%;">
+                                <label class="btn btn-primary not-active" style="width:50%"><input type="radio" name="sexoption" id="option1" value="M" required>Homem</label>
+                                <label class="btn btn-primary not-active" style="width:50%"><input type="radio" name="sexoption" id="option2" value="F" required>Mulher</label>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+	  		<div class="form-group">
+		      <label for="age">Idade</label>
+                      <input type="text" class="form-control" name="age" onkeypress="return SomenteNumeros(event);" maxlength="3" required>
+		    </div>
+
+	  		<div class="form-group">
+		      <label for="weight">Peso (Kg)</label>
+                      <input type="text" class="form-control" name="weight" onkeypress="return SomenteNumero(event);" maxlength="6" required>
+		    </div>
+
+		    <div class="form-group">
+		      <label for="height">Altura (Centímetros)</span>
+		      <input type="text" class="form-control"  name="height" onkeypress="return SomenteNumero(event);" maxlength="6" required>
+		    </div>
+
+                    
+                <div class="text-center">
+                    <input type="hidden" class="form-control" id="bmrCheck" name="bmibmr" value="bmr">
+                    <button type="submit" class="btn btn-default btn-primary calcbtn">Calcular</button>
+                </div>
+            </div>
+        </div>
+    </form>
+    <div class="calcValues"></div>
+    <div id="calcFront" data-calcFront="<?php echo plugins_url('includes/front_calc.php', __FILE__); ?>" ></div>
+    <div id="bmibmrRes" data-saveBDataURL="<?php echo plugins_url('bmibmr_saved.php', __FILE__); ?>" style="clear:both; margin-top:26px; border-top:3px solid #B4B4B4;width: 100%; padding-top: 12px;" >
+    </div>
+</div>
